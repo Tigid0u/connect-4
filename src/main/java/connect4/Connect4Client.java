03260f2fb4abd3954597e00228b5680af30e0fa3
Nolan <nolan.evard@heig-vd.ch>
2025-11-26 21:36:47 +0100

    WIP on the client
    The client commands might change slightly

--- src/main/java/connect4/Connect4Client.java
+++ src/main/java/connect4/Connect4Client.java
@@ -4,15 +4,27 @@ import connect4.utils.*;
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine;
 
-public class Connect4Client {
-  private static final String HOST = "localhost";
-  private static final int PORT = 1234;
+@Command(version = "Connect4Client v1.0", mixinStandardHelpOptions = true)
+
+public class Connect4Client implements Runnable {
   public static String END_OF_LINE = "\n";
+  private String username;
 
-  public static void main(String[] args) {
-    System.out.println("[Client] Connecting to " + HOST + ":" + PORT + "...");
-    try (Socket socket = new Socket(HOST, PORT);
+  @Option(names = { "-h",
+      "--hostname" }, description = "Hostname of the server. Default is ${DEFAULT-VALUE}", defaultValue = "localhost")
+  private String hostname;
+
+  @Option(names = { "-p",
+      "--port" }, description = "Port of the server. Default is ${DEFAULT-VALUE}", defaultValue = "4444")
+  private int port;
+
+  public void run() {
+    System.out.println("[Client] Connecting to " + hostname + ":" + port + "...");
+    try (Socket socket = new Socket(hostname, port);
         Reader reader = new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8);
         BufferedReader in = new BufferedReader(reader);
         Writer writer = new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8);
@@ -20,14 +32,21 @@ public class Connect4Client {
         Reader systemInReader = new InputStreamReader(System.in, StandardCharsets.UTF_8);
         BufferedReader bsir = new BufferedReader(systemInReader)) {
 
-      // TODO: show game menu
+      CliClientUtils.displayBanner();
 
       while (!socket.isClosed()) {
-        // TODO: to complete
+        // TODO: Get user input
+        username = CliClientUtils.getUserInput("Please type in your username");
+
       }
 
     } catch (Exception e) {
-      // TODO: handle errors
+      System.out.println("[Client] Exception: " + e.getMessage());
     }
+  }
+
+  public static void main(String[] args) {
+    int exitCode = new CommandLine(new Connect4Client()).execute(args);
+    System.exit(exitCode);
   }
 }
---
+++ src/main/java/connect4/utils/CliClientUtils.java
@@ -0,0 +1,64 @@
+package connect4.utils;
+
+import java.util.Scanner;
+
+public class CliClientUtils {
+  private static final Scanner sin = new Scanner(System.in);
+
+  /**
+   * Displays connect 4 logo as a banner
+   **/
+  public static void displayBanner() {
+    String header = """
+        ╔════════════════════════════════════════════════════════════════════════════════════════════╗
+        ║                                                                                            ║
+        ║     █████████                                                    █████       █████ █████   ║
+        ║    ███░░░░░███                                                  ░░███       ░░███ ░░███    ║
+        ║   ███     ░░░   ██████  ████████   ████████    ██████   ██████  ███████      ░███  ░███ █  ║
+        ║  ░███          ███░░███░░███░░███ ░░███░░███  ███░░███ ███░░███░░░███░       ░███████████  ║
+        ║  ░███         ░███ ░███ ░███ ░███  ░███ ░███ ░███████ ░███ ░░░   ░███        ░░░░░░░███░█  ║
+        ║  ░░███     ███░███ ░███ ░███ ░███  ░███ ░███ ░███░░░  ░███  ███  ░███ ███          ░███░   ║
+        ║   ░░█████████ ░░██████  ████ █████ ████ █████░░██████ ░░██████   ░░█████           █████   ║
+        ║    ░░░░░░░░░   ░░░░░░  ░░░░ ░░░░░ ░░░░ ░░░░░  ░░░░░░   ░░░░░░     ░░░░░           ░░░░░    ║
+        ║                                                                                            ║
+        ╚════════════════════════════════════════════════════════════════════════════════════════════╝""";
+    System.out.println(header);
+  }
+
+  public static void requestEnter() {
+    try (Scanner sin = new Scanner(System.in);) {
+      sin.nextLine();
+    } catch (Exception e) {
+      System.out.println("[requestEnter]: " + e.getMessage());
+    }
+  }
+
+  /**
+   * Retrieves user input and prints request message
+   *
+   * @param message the message to be displayed to the user
+   *
+   * @return the input of the user as a string
+   **/
+  public static String getUserInput(String message) {
+    System.out.print(message + " > ");
+
+    return sin.nextLine();
+  }
+
+  // Source - https://stackoverflow.com/a
+  // Posted by satish, modified by community. See post 'Timeline' for change
+  // history
+  // Retrieved 2025-11-26, License - CC BY-SA 4.0
+  public static void clearScreen() {
+    System.out.print("\033[H\033[2J");
+    System.out.flush();
+  }
+
+  public static void main(String[] args) {
+    displayBanner();
+    String res = getUserInput("Please type in your username");
+    clearScreen();
+    getUserInput("");
+  }
+}
